bde3dfa05de133005ffdcb6bf3da4154f1704a5c
Casey Leask <caseyleask@gmail.com>
2018-08-16 12:16:13 +1000

    Workaround for failures in `app` dataset events

--- lib/honeycomb-rails/railtie.rb
+++ lib/honeycomb-rails/railtie.rb
@@ -19,7 +19,7 @@ module HoneycombRails
     config.after_initialize do
       HoneycombRails.config.logger ||= ::Rails.logger
 
-      @libhoney = HoneycombRails.config.client || begin
+      @req_libhoney = HoneycombRails.config.client || begin
         writekey = HoneycombRails.config.writekey
         default_dataset = HoneycombRails.config.dataset
         if writekey.blank? || default_dataset.blank?
@@ -30,18 +30,34 @@ module HoneycombRails
           Libhoney::Client.new(
             writekey: writekey,
             dataset: default_dataset,
-            user_agent_addition: HoneycombRails::USER_AGENT_SUFFIX,
+            user_agent_addition: HoneycombRails::USER_AGENT_SUFFIX
+          )
+        end
+      end
+
+      @db_libhoney = HoneycombRails.config.client || begin
+        writekey = HoneycombRails.config.writekey
+        default_dataset = HoneycombRails.config.db_dataset
+        if writekey.blank? || default_dataset.blank?
+          missing = writekey.blank? ? 'writekey' : 'dataset'
+          warn "No #{missing} defined! (Check your config's `#{missing}` value in config/initializers/honeycomb.rb) No events will be sent to Honeycomb."
+          Libhoney::NullClient.new
+        else
+          Libhoney::Client.new(
+            writekey: writekey,
+            dataset: default_dataset,
+            user_agent_addition: HoneycombRails::USER_AGENT_SUFFIX
           )
         end
       end
     end
 
     config.after_initialize do
-      req_builder = @libhoney.builder
+      req_builder = @req_libhoney.builder
       Subscribers::ProcessAction.new(req_builder).subscribe!
 
       if !HoneycombRails.config.db_dataset.blank?
-        db_builder = @libhoney.builder
+        db_builder = @db_libhoney.builder
         db_builder.dataset = HoneycombRails.config.db_dataset
         Subscribers::ActiveRecord.new(db_builder).subscribe!
       end
